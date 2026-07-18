08a5d3c608d2e1d41a9fdab4de56805173c4a3d1
fayziyevgenius <fayzibekfayziyev11@gmail.com>
2026-07-18 14:48:01 +0500

    Update page.tsx

---
+++ app/edit-profile/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function EditProfilePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white">
+      <h1 className="text-4xl font-bold">
+        Edit Profile (Coming Soon)
+      </h1>
+    </div>
+  );
+}
